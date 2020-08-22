ca5baaadd0361d36d32985461e7bcb55ce6315ff
Muhaimin Jobayer <mahmudmuhaimin@yahoo.com>
2020-08-22 02:52:11 -0500

    Accordion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import './App.css';
-import Messages from './Messages';
-import TheDate from './state/TheDate'
+import Tabs from './state/Tabs';
 
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
-function App() {
-  return (
-    <main className='App'>
-      <Messages name="Messages" unread={0}/>
-      <Messages name="Notifications" unread={10}/>
-      <TheDate />
-    </main>
-  )
+class App extends React.Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+  }
 }
 
 export default App;
